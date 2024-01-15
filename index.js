bf4e736ebacb8e940634c4c54209a13c0fd1a62d
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-01-15 19:31:51 +0700

    dynamic

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("views"))
-app.use(express.static("public"))
+app.use('/dynamic-images', express.static('assets'));
 
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirname, 'views'));
